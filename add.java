bdb803f6064cce7d35d5f0ee893370e57a114e7d
vaishnvisusheel <vaishnaviambarkhed@gmail.com>
2025-04-09 14:28:36 +0530

    save

--- add.java
+++ add.java
@@ -1,7 +1,12 @@
+package java;
 public class addition{
+
     public static void main(String[]args){
-        int a=10;
-        int b=20;
-        system.out.println("sum is"+sum);
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        int sub = a-b;
+        system.out.println("sum is" +sum);
+        system.out.println("sub is" +sub);
     }
 }
